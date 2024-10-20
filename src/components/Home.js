7a9ab7fe27c490f8f7c4b888ce971db1837ea7a5
David <okeahdavid@gmail.com>
2024-10-20 20:05:43 +0200

    updated Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,26 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Box, Typography } from '@mui/material';
 import { TypingEffect } from './TypingEffect';
 import '../styles/Home.css';
 
 const Home = () => {
-  const [isSpinning, setIsSpinning] = useState(false);
-
-  useEffect(() => {
-    if (isSpinning) {
-      const timer = setTimeout(() => {
-        setIsSpinning(false);
-      }, 1000); // 1000ms = 1 second, matching the animation duration
-      return () => clearTimeout(timer);
-    }
-  }, [isSpinning]);
-
-  const handleImageClick = () => {
-    if (!isSpinning) {
-      setIsSpinning(true);
-    }
-  };
-
   return (
     <Box id="home" className="home-container">
       <Typography variant="h1" className="typography-h1">
@@ -28,9 +11,8 @@ const Home = () => {
       </Typography>
       <img 
         src="/profile_picture.jpeg" 
-        alt="David" // Changed this line
-        className={`profile-image ${isSpinning ? 'spinning' : ''}`}
-        onClick={handleImageClick}
+        alt="David"
+        className="profile-image"
       />
     </Box>
   );
